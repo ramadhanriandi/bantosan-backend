c03c6d671e72c076316729ae4b4a211faebde91a
ramadhanriandi <izurbudianto@gmail.com>
2020-07-23 15:53:27 +0700

    add post disaster API and fix bug in get all disasters API

---
+++ app/src/main/java/com/blibli/demo/company/model/command/CreateDisasterRequest.java
@@ -0,0 +1,37 @@
+package com.blibli.demo.company.model.command;
+
+import com.blibli.demo.company.entity.Location;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateDisasterRequest {
+  @NotBlank
+  private String name;
+
+  @NotBlank
+  private String category;
+
+  @NotBlank
+  private String status;
+
+  @NotBlank
+  private String description;
+
+  @NotNull
+  private Location location;
+
+  @NotBlank
+  private String evidence;
+
+  @NotBlank
+  private String reporter;
+}
