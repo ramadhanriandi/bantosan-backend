488813bedf6cb845aa23ca0d182b553f07ad2dc7
ramadhanriandi <izurbudianto@gmail.com>
2020-07-25 11:42:08 +0700

    add get all donations API

--- app/src/main/java/com/blibli/demo/company/controller/DonationController.java
+++ app/src/main/java/com/blibli/demo/company/controller/DonationController.java
@@ -1,10 +1,14 @@
 package com.blibli.demo.company.controller;
 
 import com.blibli.demo.base.BaseResponse;
+import com.blibli.demo.base.ListBaseResponse;
 import com.blibli.demo.base.SingleBaseResponse;
 import com.blibli.demo.company.entity.Donation;
 import com.blibli.demo.company.model.command.CreateDonationRequest;
 import com.blibli.demo.company.model.command.UpdateDonationRequest;
+import com.blibli.demo.company.model.web.DonaturResponse;
+import com.blibli.demo.company.model.web.FundraisingResponse;
+import com.blibli.demo.company.model.web.GetAllDonationsResponse;
 import com.blibli.demo.company.model.web.UpdateDonationResponse;
 import com.blibli.demo.company.security.service.DonationService;
 import org.springframework.beans.BeanUtils;
@@ -15,6 +19,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -22,6 +28,46 @@ import javax.validation.Valid;
 public class DonationController {
   @Autowired
   private DonationService donationService;
+
+  @RequestMapping(
+          method = RequestMethod.GET,
+          produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+  public ResponseEntity getAllDonation(
+          @RequestParam(value = "userId", required = false) String userId,
+          @RequestParam(value = "fundraisingId", required = false) String fundraisingId
+  ) throws Exception {
+    List<Donation> donations = this.donationService.find(userId, fundraisingId);
+    List<GetAllDonationsResponse> donationsResponses = new ArrayList<>();
+
+    for (Donation donation : donations) {
+      GetAllDonationsResponse donationsResponse = GetAllDonationsResponse.builder().build();
+      BeanUtils.copyProperties(donation, donationsResponse);
+
+      DonaturResponse donaturResponse = DonaturResponse.builder().build();
+      BeanUtils.copyProperties(donation.getDonatur(), donaturResponse);
+      donationsResponse.setDonatur(donaturResponse);
+
+      FundraisingResponse fundraisingResponse = FundraisingResponse.builder().build();
+      BeanUtils.copyProperties(donation.getFundraising(), fundraisingResponse);
+      donationsResponse.setFundraising(fundraisingResponse);
+
+      donationsResponses.add(donationsResponse);
+    }
+
+    return ResponseEntity.ok(
+            new ListBaseResponse<>(
+                    null,
+                    null,
+                    true,
+                    null,
+                    donationsResponses,
+                    null
+            )
+    );
+  }
+
 
   @RequestMapping(
           method = RequestMethod.POST,
